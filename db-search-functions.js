e2207e7a47e474f11f3293db0547e180d4198885
undefined8 <dzamilatskiy2019@bklawtech.com>
2017-11-13 22:09:41 +0000

    Find and Go Seek

--- db-search-functions.js
+++ db-search-functions.js
@@ -10,39 +10,110 @@ function getVersion(handleVersion) {
     // the database snapshot and passes it as an argument to the 'handleVersion'
     // callback.
     
-    //var version = snapshot.val();
-    //handleVersion(version);
+    var version = snapshot.val();
+    handleVersion(version);
   });
 }
 
 function getHelloWorldValue(handleType) {
   // TODO: Your code goes here. Hint: use 'getVersion' as a template.
+  
+  var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/hello-world');
+  
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    var value = snapshot.val();
+    handleType(value);
+  });
 }
 
 function applyDiscount(handleNewPrice) {
   // TODO: Your code goes here.
+  var database = firebase.database();
+  var databaseReference = database.ref('/db-search/discount');
+  
+  databaseReference.once('value').then(function(snapshot) {
+    
+    var discount = snapshot.val();
+    
+    handleNewPrice(discount * 100);
+  });
+  
 }
 
 function getArrayLength(handleLength) {
   // TODO: Your code goes here.
+  var database = firebase.database();
+  var databaseReference = database.ref('/db-search/array');
+  
+  databaseReference.once('value').then(function(snapshot) {
+    
+    var array = snapshot.val();
+    handleLength(array.length);
+  });
 }
 
 function getLastArrayElement(handleElement) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseReference = database.ref('/db-search/array');
+  
+  databaseReference.once('value').then(function(snapshot) {
+    
+    var array = snapshot.val();
+    handleElement(array[array.length - 1]);
+  });
 }
 
 function getArrayJoin(handleJoin) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseReference = database.ref('/db-search/array');
+  
+  databaseReference.once('value').then(function(snapshot) {
+    
+    var array = snapshot.val();
+    handleJoin(array.join(' '));
+  });
 }
 
 function getNumberOrStringType(handleType) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseReference = database.ref('/db-search/number-or-string');
+  
+  databaseReference.once('value').then(function(snapshot) {
+    
+    var value = snapshot.val();
+    handleType(typeof(value));
+  });
 }
 
 function getObjectValue(handleValue) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseReference = database.ref('/db-search/object');
+  
+  databaseReference.once('value').then(function(snapshot) {
+    
+    var object = snapshot.val();
+    handleValue(object.donkey);
+  });
 }
 
 function getObjectKeyCount(handleKeyCount) {
   // TODO: Your code goes here.
+  var database = firebase.database();
+  var databaseReference = database.ref('/db-search/object');
+  
+  databaseReference.once('value').then(function(snapshot) {
+    
+    var object = snapshot.val();
+    handleKeyCount(Object.keys(object).length);
+  });
 }
